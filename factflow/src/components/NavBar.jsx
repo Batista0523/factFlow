ed71be266043e9e178573877e640610a64afdd5c
elisaul batista <elisaulbatista@pursuit.org>
2023-10-17 21:17:49 -0400

    setting up the components

---
+++ factflow/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className='navbar'>
+            <div className="nav-branch">FactFlow</div>
+            <button className="create-btn">Create New Resource</button>
+        </div>
+    );
+}
+
+export default NavBar;
