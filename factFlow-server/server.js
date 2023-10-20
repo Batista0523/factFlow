24872a6cc8151d1fb05b0144aa3aefcad64cb741
elisaul batista <elisaulbatista@pursuit.org>
2023-10-20 12:56:42 -0400

    working in the index page component

--- factFlow-server/server.js
+++ factFlow-server/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const cors = require('cors');  
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}));
 
 
 const transactionsRouter = require('./routes/transactions');
--- factflow/src/components/IndexPage.jsx
+++ factflow/src/components/IndexPage.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-
-const IndexPage = () => {
-    return (
-        <div className='index'>
-           <p>welcome to Fact-Flow</p>
-        </div>
-    );
-}
+const IndexPage = ({ transactions }) => {
+  console.log(transactions);
+  return (
+    <div className="index">
+      <h1>Transactions</h1>
+      <p>Welcome to Fact-Flow</p>
+      {Array.isArray(transactions) && transactions.length > 0 ? (
+        <ul>
+          {transactions.map((transaction) => (
+            <li key={transaction.id}>
+              <p>Item name: {transaction.item_name}</p>
+              <p>Amunt : {transaction.amount}</p>
+              <p>Date: {transaction.date}</p>
+              <p>Category: {transaction.category}</p>
+              <p>From: {transaction.from}</p>
+              {transaction.income ? <p>Income</p> : <p>Expense</p>}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No transactions available.</p>
+      )}
+    </div>
+  );
+};
 
 export default IndexPage;
