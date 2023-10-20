e5ca5ebbde3a94c55a11df7dc2cb9742a14b9401
elisaul batista <elisaulbatista@pursuit.org>
2023-10-20 13:20:13 -0400

    according to the requirement

--- factflow/src/components/IndexPage.jsx
+++ factflow/src/components/IndexPage.jsx
@@ -1,21 +1,41 @@
 import React, { useState, useEffect } from "react";
 
 const IndexPage = ({ transactions }) => {
-  console.log(transactions);
+
+  console.log(transactions)
+  let totalAmount = 0; 
+
+  if (Array.isArray(transactions) && transactions.length > 0) {
+    totalAmount = transactions.reduce((total, transaction) => total + transaction.amount, 0);
+  }
+
+  const getTotalColor = () => {
+    if (totalAmount > 100) {
+      return 'green';
+    } else if (totalAmount >= 0) {
+      return 'yellow';
+    } else {
+      return 'red';
+    }
+  };
+
   return (
-    <div className="index">
+    <div className={`index ${getTotalColor()}`}>
       <h1>Transactions</h1>
       <p>Welcome to Fact-Flow</p>
+      <p>Total: {totalAmount}</p>
       {Array.isArray(transactions) && transactions.length > 0 ? (
         <ul>
           {transactions.map((transaction) => (
             <li key={transaction.id}>
-              <p>Item name: {transaction.item_name}</p>
-              <p>Amunt : {transaction.amount}</p>
+               <p>Item name: {transaction.item_name}</p>
+              <p>Amount: {transaction.amount}</p>
               <p>Date: {transaction.date}</p>
               <p>Category: {transaction.category}</p>
               <p>From: {transaction.from}</p>
               {transaction.income ? <p>Income</p> : <p>Expense</p>}
+              <button>Edit</button>
+              <button>Delete</button>
             </li>
           ))}
         </ul>
@@ -25,5 +45,6 @@ const IndexPage = ({ transactions }) => {
     </div>
   );
 };
+
 
 export default IndexPage;
